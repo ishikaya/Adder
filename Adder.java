3d98f8e4aef52d35657efecb26a5d7bf9682e250
ishikaya <60050246@kmitl.ac.th>
2018-09-10 18:52:43 +0700

    add : Adder.java

---
+++ Adder.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Adder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("A = ");
+        int a = sc.nextInt();
+        System.out.print("B = ");
+        int b = sc.nextInt();
+        int c = a+b;    
+    System.out.println("Answer = "+c);
+    }
+}
